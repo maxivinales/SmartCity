e6a2fc6dc580b044298d25f069869e31ba668017
lucascukla <lucascukla1994@gmail.com>
2023-07-20 11:55:50 -0300

    Parece que anda el filtro :) el tiempo de filtrado es de 876 nA, lo que supone el 4.2048% del tiempo de procesamiento de 1 procesador, considerando que la frecuencia de muestreo es 48 kHz

--- main/aux_task.c
+++ main/aux_task.c
@@ -1,14 +1,14 @@
 #include "aux_task.h"
 #include <math.h>
 
-#define _f 100.0 
+#define _f 8000.0 
 
 #define K_Aw 1.189276456832886
 #define K_Cw 1.004441738128662
 
 TaskHandle_t TaskHandle_aux;                                    // "variable para manejar las tareas"
 
-cmd_audio_weighting_t ponderacion = C;
+cmd_audio_weighting_t ponderacion = A;
 
 extern void filtro_II_d_I(float* muestra_p, float* _x, float* _y, float* _SOS);    // filtro
 extern void casting_y_escala(int muestra_cuentas, float* muestra_p, float* k_veces_to_p);
@@ -17,8 +17,9 @@ extern void test_asm(float* x, float* y, float* z);
 
 // Ponderacion tipo A
 const struct filtro_IIR_2ord A_weighting_0 = {.b_0 = 0.197012037038803, .b_1 = 0.394024074077606, .b_2 = 0.197012037038803, .a_1 = -0.224558457732201, .a_2 = 0.012606625445187, .k_f = 1};
-const struct filtro_IIR_2ord A_weighting_1 = {.b_0 = 1, .b_1 = -2, .b_2 = 1, .a_1 = -1.994614481925964, .a_2 = 0.994621694087982, .k_f = 1};
-const struct filtro_IIR_2ord A_weighting_2 = {.b_0 = 1, .b_1 = -2, .b_2 = 1, .a_1 = -1.893870472908020, .a_2 = 0.895159780979157, .k_f = 1};
+const struct filtro_IIR_2ord A_weighting_1 = {.b_0 = 1, .b_1 = -2, .b_2 = 1, .a_1 = -1.893870472908020, .a_2 = 0.895159780979157, .k_f = 1};
+const struct filtro_IIR_2ord A_weighting_2 = {.b_0 = 1, .b_1 = -2, .b_2 = 1, .a_1 = -1.994614481925964, .a_2 = 0.994621694087982, .k_f = 1};
+
 
 void aux_task(void *parameter){
     printf("Iniciando aux_task\n"); 
@@ -68,11 +69,36 @@ void aux_task(void *parameter){
         *(y_3+i) = 0;
     }
 
+    // printf("b00 = %f\n", 1000000000.0*punt->b_0);
+    // printf("b10 = %f\n", 1000000000.0*punt->b_1);
+    // printf("b20 = %f\n", 1000000000.0*punt->b_2);
+    // printf("a10 = %f\n", 1000000000.0*punt->a_1);
+    // printf("a20 = %f\n", 1000000000.0*punt->a_2);
+    // punt++;
+    // printf("b01 = %f\n", 1000000000.0*punt->b_0);
+    // printf("b11 = %f\n", 1000000000.0*punt->b_1);
+    // printf("b21 = %f\n", 1000000000.0*punt->b_2);
+    // printf("a11 = %f\n", 1000000000.0*punt->a_1);
+    // printf("a21 = %f\n", 1000000000.0*punt->a_2);
+    // punt++;
+    // printf("b02 = %f\n", 1000000000.0*punt->b_0);
+    // printf("b12 = %f\n", 1000000000.0*punt->b_1);
+    // printf("b22 = %f\n", 1000000000.0*punt->b_2);
+    // printf("a12 = %f\n", 1000000000.0*punt->a_1);
+    // printf("a22 = %f\n", 10000000000.0*punt->a_2);
+    // punt = aux_punt;
+
+    // printf("tamaño flotante = %d\n", sizeof(punt->b_0));
+
+    int aux1 = 10;
+    float aux2 = 1, aux3 = 1;
+
+    uint32_t tiempo;
     while (1)
     {
-        /*
         seno = _A*sin(omega_m*n);
-
+        tiempo = esp_cpu_get_cycle_count();             // tomo los ciclos del procesador
+        casting_y_escala(aux1, &aux2, &aux3);
         if(ponderacion != Z){
             filtro_II_d_I(&seno, x_1, y_1, &punt->b_0);
             punt++;
@@ -86,21 +112,20 @@ void aux_task(void *parameter){
             }
             punt = aux_punt;
         }
+        tiempo = esp_cpu_get_cycle_count() - tiempo;             // tomo los ciclos del procesador
 
 
         // casting_y_escala(&enterito, &flotantesito, _k_veces_to_p);
         
         
-        printf("%f\t%f\n", seno, k_cw*(*y_1));
+        // printf("%f\t%f\n", seno, k_aw*(*y_3));
+        printf("tiempo procesamiento: %ld ns\n", 4*tiempo);
 
        n++;
 
-
-        */
-
-        test_asm(&tuvieja, &tuhermana, &kaka);
-        printf("x = %f\ty = %f\t z = %f\n", tuvieja, tuhermana, kaka);
-        vTaskDelay(pdMS_TO_TICKS(100));
+        // test_asm(&tuvieja, &tuhermana, &kaka);
+        // printf("x = %f\ty = %f\t z = %f\n", tuvieja, tuhermana, kaka);
+        vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
 
