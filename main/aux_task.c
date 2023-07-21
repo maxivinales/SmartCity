46a292c33799cfc25f46e37a6582cac67e2b2461
lucascukla <lucascukla1994@gmail.com>
2023-07-21 09:04:17 -0300

    funcionando el test del filtro aparentemente, pero dando mal los parámetros del filtro. Voy a hacer una prueba mas empírica graficando 2 senos a diferentes frecuencias.

--- main/aux_task.c
+++ main/aux_task.c
@@ -1,4 +1,5 @@
 #include "aux_task.h"
+#include "freertos/projdefs.h"
 #include <math.h>
 
 #define _f 8000.0 
@@ -28,104 +29,10 @@ float k_aw = K_Aw;
 void aux_task(void *parameter){
     printf("Iniciando aux_task\n"); 
 
-    // Parametros del filtro
-
-    float seno = 0;
-
-    const float omega_m = 2.0*MPI*_f/48000.0;
-    const float _A = 1.0;
-
-    float n = 0;
-
-    float acumulador = 0;
-    
-    struct filtro_IIR_2ord *punt = malloc(3*sizeof(struct filtro_IIR_2ord));    // reservo memoria para tener todos los parametros del filtro en direcciones contiguas
-    struct filtro_IIR_2ord *aux_punt;                                               // es para no perder la direccion donde empiezan los parametros del filtro
-
-    aux_punt = punt;
-    // guardo en memoria los parametros del filtro
-    *punt = A_weighting_0;
-    *(punt + 1) = A_weighting_1;
-    *(punt + 2) = A_weighting_2;
-
-    float *x_1, *x_2, *x_3, *y_1, *y_2, *y_3; // buffers de x[n] e y[n]
-    x_1 = malloc(3*sizeof(float));
-    x_2 = malloc(3*sizeof(float));
-    x_3 = malloc(3*sizeof(float));
-    y_1 = malloc(3*sizeof(float));
-    y_2 = malloc(3*sizeof(float));
-    y_3 = malloc(3*sizeof(float));
-
-    float *xa, *xb, *xc, *ya, *yb, *yc;
-    xa = malloc(3*sizeof(float));
-    xb = malloc(3*sizeof(float));
-    xc = malloc(3*sizeof(float));
-    ya = malloc(3*sizeof(float));
-    yb = malloc(3*sizeof(float));
-    yc = malloc(3*sizeof(float));
-
-    float _k_veces_to_p = 0.000003695;
-
-    float tuvieja = 1;
-    float tuhermana = 0.2;
-    float kaka = 0.3;
-
-    for(int i = 0; i<3; i++){
-        *(x_1+i) = 0;
-        *(x_2+i) = 0;
-        *(x_3+i) = 0;
-        *(y_1+i) = 0;
-        *(y_2+i) = 0;
-        *(y_3+i) = 0;
-
-        *(xa+i) = 0;
-        *(xb+i) = 0;
-        *(xc+i) = 0;
-        *(ya+i) = 0;
-        *(yb+i) = 0;
-        *(yc+i) = 0;
-    } // voy a probar el filtro en el void
-
-    int aux1 = 10;
-    float aux2 = 1, aux3 = 1;
-
-    uint32_t tiempo;
-
     test_filtro();
+
     while (1)
     {
-        /*
-        seno = _A*sin(omega_m*n);
-        tiempo = esp_cpu_get_cycle_count();             // tomo los ciclos del procesador
-        casting_y_escala(aux1, &aux2, &aux3);
-        if(ponderacion != Z){
-            filtro_II_d_I(&seno, x_1, y_1, &punt->b_0);
-            punt++;
-            filtro_II_d_I(y_1, x_2, y_2, &punt->b_0);
-            if(ponderacion == A){
-                punt++;
-                filtro_II_d_I(y_2, x_3, y_3, &punt->b_0);
-                producto_y_acumulacion(y_3, &acumulador, &k_aw);
-            }else{
-                producto_y_acumulacion(y_2, &acumulador, &k_cw);
-            }
-            punt = aux_punt;
-        }
-        tiempo = esp_cpu_get_cycle_count() - tiempo;             // tomo los ciclos del procesador
-
-
-        // casting_y_escala(&enterito, &flotantesito, _k_veces_to_p);
-        
-        
-        // printf("%f\t%f\n", seno, k_aw*(*y_3));
-        printf("tiempo procesamiento: %ld ns\n", 4*tiempo);
-
-       n++;
-
-        // test_asm(&tuvieja, &tuhermana, &kaka);
-        // printf("x = %f\ty = %f\t z = %f\n", tuvieja, tuhermana, kaka);
-
-        */
         vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
@@ -159,7 +66,7 @@ void test_filtro(void){
     ya = malloc(3*sizeof(float));
     yb = malloc(3*sizeof(float));
     yc = malloc(3*sizeof(float));
-    printf("linea 168");
+
     for(int i=0; i<3; i++){
         *(x0+i) = 0;
         *(x1+i) = 0;
@@ -185,11 +92,11 @@ void test_filtro(void){
     acumulador_seno = 0;
     acumulador_A = 0;
     acumulador_C = 0;
-    printf("linea 194");
+    
     for(int i=0; i<34; i++){
         omega = 2.0*MPI*frecuencias[i]/48000.0;
-        printf("linea 197");
-        int N_muestras = (int)(frecuencias[i]*15*F_sample);    // defino la cantidad de muestras para 15 periodos
+        
+        int N_muestras = (int)(15*F_sample/frecuencias[i]);    // defino la cantidad de muestras para 15 periodos
         for(int j=0;j<N_muestras;j++){
             seno_int = (int)(10000.0*sin(omega*j));
             // Ponderacion A
@@ -212,11 +119,13 @@ void test_filtro(void){
             acumulador_seno = acumulador_seno +  seno*seno;       // acumulo para calcular el rms en 1 s
         }
 
-        atenuacion_A_nuestra[i] = 20.0*log10((acumulador_A)/(acumulador_seno));
-        atenuacion_C_nuestra[i] = 20.0*log10((acumulador_C)/(acumulador_seno));
-        // printf("\n\nciclos_A = %d\tciclos_C = %d\n\n", ciclos_A, ciclos_C);
-        // vTaskDelay(pdMS_TO_TICKS(20));
-        printf("dBA_filtro\tdBA_IRAM\tdBC_flitro\tdBC_IRAM\n");
+        atenuacion_A_nuestra[i] = 20.0*log10(sqrt((acumulador_A)/(acumulador_seno)));
+        atenuacion_C_nuestra[i] = 20.0*log10(sqrt((acumulador_C)/(acumulador_seno)));
+        vTaskDelay(pdMS_TO_TICKS(20));
+    }
+    printf("ciclos_A = %d\tciclos_C = %d\n", ciclos_A, ciclos_C);
+    printf("dBA_filtro\tdBA_IRAM\tdBC_flitro\tdBC_IRAM\n");
+    for(int i=0; i<34; i++){
         vTaskDelay(pdMS_TO_TICKS(20));
         printf("%f\t%f\t%f\t%f\n", atenuacion_A_nuestra[i], atenuacion_A_IRAM[i], atenuacion_C_nuestra[i], atenuacion_C_IRAM[i]);
     }
