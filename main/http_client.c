03398db90e8b38203685e247894236816ea907b4
maxivinales47 <maximiliano.vinales@faniot.com.ar>
2023-09-18 20:28:39 -0300

    Se agrego funcion add_to_json

--- main/http_client.c
+++ main/http_client.c
@@ -14,7 +14,7 @@ struct sensor sensor_data;
 //struct sensor ;
 //struct sensor //etc 
 
-cJSON* createTemperatureJSON(struct sensor* sensor_data);
+cJSON* add_to_JSON(struct sensor* sensor_data);
 const char *TAG_http_client = "HTTTP client";
 
 bool http_receiving = false;
@@ -24,101 +24,101 @@ esp_err_t client_event_get_handler(esp_http_client_event_handle_t evt)
 switch (evt->event_id)
 {
 case HTTP_EVENT_ON_DATA:
-    printf("HTTP_EVENT_ON_DATA: %d, %s\n", evt->data_len, (char *)evt->data);
-    if (evt->data_len > 0) {
-        if (http_receiving == false) {
-            http_receiving = true;
-            recv_http = malloc((evt->data_len + 1) * sizeof(char)); // +1 para el carácter nulo
-            if (recv_http == NULL) {
-                ESP_LOGE(TAG_http_client, "Error de asignación de memoria\n");
-                return ESP_FAIL;
-            }
-            strcpy(recv_http, (char *)evt->data);
-        } else {
-            char *aux_S1;
-            size_t len_string = strlen(recv_http) + 1 + evt->data_len + 1; // +1 para el carácter nulo
-            aux_S1 = malloc(len_string);
-            if (aux_S1 == NULL) {
-                ESP_LOGE(TAG_http_client, "Error de asignación de memoria\n");
-                return ESP_FAIL;
-            }
-            snprintf(aux_S1, len_string, "%s%s", recv_http, (char *)evt->data);
-            free(recv_http);
-            recv_http = aux_S1;
-        }
-    }
-    break;
+printf("HTTP_EVENT_ON_DATA: %d, %s\n", evt->data_len, (char *)evt->data);
+if (evt->data_len > 0) {
+if (http_receiving == false) {
+http_receiving = true;
+recv_http = malloc((evt->data_len + 1) * sizeof(char)); // +1 para el carácter nulo
+if (recv_http == NULL) {
+    ESP_LOGE(TAG_http_client, "Error de asignación de memoria\n");
+    return ESP_FAIL;
+}
+strcpy(recv_http, (char *)evt->data);
+} else {
+char *aux_S1;
+size_t len_string = strlen(recv_http) + 1 + evt->data_len + 1; // +1 para el carácter nulo
+aux_S1 = malloc(len_string);
+if (aux_S1 == NULL) {
+    ESP_LOGE(TAG_http_client, "Error de asignación de memoria\n");
+    return ESP_FAIL;
+}
+snprintf(aux_S1, len_string, "%s%s", recv_http, (char *)evt->data);
+free(recv_http);
+recv_http = aux_S1;
+}
+}
+break;
 case HTTP_EVENT_ON_FINISH:
-    printf("HTTP_EVENT_ON_FINISH: %s\n", recv_http);
-    if (http_receiving == true) {
-        http_receiving = false;
-        cJSON *Jsonsito = cJSON_Parse(recv_http);
+printf("HTTP_EVENT_ON_FINISH: %s\n", recv_http);
+if (http_receiving == true) {
+http_receiving = false;
+cJSON *Jsonsito = cJSON_Parse(recv_http);
 
-        if (Jsonsito == NULL) {
-            ESP_LOGW(TAG_http_client, "Error al analizar el JSON\n");
-            const char *error_ptr = cJSON_GetErrorPtr();
-            if (error_ptr != NULL) {
-                printf("Error: %s\n", error_ptr);
-            }
-            free(recv_http); // Libera la memoria antes de salir
-            return ESP_FAIL;
-        }
+if (Jsonsito == NULL) {
+ESP_LOGW(TAG_http_client, "Error al analizar el JSON\n");
+const char *error_ptr = cJSON_GetErrorPtr();
+if (error_ptr != NULL) {
+    printf("Error: %s\n", error_ptr);
+}
+free(recv_http); // Libera la memoria antes de salir
+return ESP_FAIL;
+}
 
-        cJSON *firmware_version = cJSON_GetObjectItemCaseSensitive(Jsonsito, "firmware_version");
-        if (firmware_version != NULL) {
-            printf("firmware_version: %s\n", firmware_version->valuestring);
-            strcpy(new_firmware_version.value_str, firmware_version->valuestring);
-            new_firmware_version.value = 1;
-        } else {
-            ESP_LOGW(TAG_http_client, "Objeto firmware_version no encontrado o no válido\n");
-            // return ESP_FAIL;
-        }
+cJSON *firmware_version = cJSON_GetObjectItemCaseSensitive(Jsonsito, "firmware_version");
+if (firmware_version != NULL) {
+printf("firmware_version: %s\n", firmware_version->valuestring);
+strcpy(new_firmware_version.value_str, firmware_version->valuestring);
+new_firmware_version.value = 1;
+} else {
+ESP_LOGW(TAG_http_client, "Objeto firmware_version no encontrado o no válido\n");
+// return ESP_FAIL;
+}
 
-        cJSON *datetime = cJSON_GetObjectItemCaseSensitive(Jsonsito, "datetime");
-        if (datetime != NULL) {
-            printf("datetime: %s\n", datetime->valuestring);
-            strcpy(fecha_y_hora.value_str, datetime->valuestring);
-            // TRABAJAR DESDE ACA
+cJSON *datetime = cJSON_GetObjectItemCaseSensitive(Jsonsito, "datetime");
+if (datetime != NULL) {
+printf("datetime: %s\n", datetime->valuestring);
+strcpy(fecha_y_hora.value_str, datetime->valuestring);
+// TRABAJAR DESDE ACA
 
-        // Copias los primeros 10 caracteres de fecha_y_hora.value_str a fecha_k
-        strncpy(fecha_k, fecha_y_hora.value_str, 10);
-        fecha_k[10] = '\0'; // Añadis el terminador nulo para indicar el fin de la cadena
-        // Copias los caracteres del 11 al 18 de fecha_y_hora.value_str a hora_k
-        strncpy(hora_k, &fecha_y_hora.value_str[11], 8);
-        hora_k[8] = '\0'; // Añadis el terminador nulo para indicar el fin de la cadena
-        // Imprimes las cadenas resultantes
-        printf("fecha_k: %s\n", fecha_k);
-        printf("hora_k: %s\n", hora_k);
+// Copias los primeros 10 caracteres de fecha_y_hora.value_str a fecha_k
+strncpy(fecha_k, fecha_y_hora.value_str, 10);
+fecha_k[10] = '\0'; // Añadis el terminador nulo para indicar el fin de la cadena
+// Copias los caracteres del 11 al 18 de fecha_y_hora.value_str a hora_k
+strncpy(hora_k, &fecha_y_hora.value_str[11], 8);
+hora_k[8] = '\0'; // Añadis el terminador nulo para indicar el fin de la cadena
+// Imprimes las cadenas resultantes
+printf("fecha_k: %s\n", fecha_k);
+printf("hora_k: %s\n", hora_k);
 
-            //float temperature = 25.5; // Cambia esto por el valor real del sensor
-            //char* magnitud = "C"; // Cambia esto por la magnitud real del sensor
-            sensor_data.unidad="pascal";
-            sensor_data.magnitud=47.5;
+//float temperature = 25.5; // Cambia esto por el valor real del sensor
+//char* magnitud = "C"; // Cambia esto por la magnitud real del sensor
+sensor_data.unidad="pascal";
+sensor_data.magnitud=47.5;
 
-            // Crear un objeto JSON con los datos del sensor de temperatura, fecha y hora
-            cJSON* temperatureJSON = createTemperatureJSON(&sensor_data);
+// Crear un objeto JSON con los datos del sensor de temperatura, fecha y hora
+cJSON* sensorJSON = add_to_JSON(&sensor_data);
 
-            // Convertir el objeto JSON a una cadena
-            char* jsonStr = cJSON_PrintUnformatted(temperatureJSON);
+// Convertir el objeto JSON a una cadena
+char* jsonStr = cJSON_PrintUnformatted(sensorJSON);
 
-            // Imprimir el JSON resultante
-            printf("JSON resultante: %s\n", jsonStr);
+// Imprimir el JSON resultante
+printf("JSON resultante: %s\n", jsonStr);
 
-            // Liberar la memoria del objeto JSON y la cadena
-            cJSON_Delete(temperatureJSON);
-            free(jsonStr);
+// Liberar la memoria del objeto JSON y la cadena
+cJSON_Delete(sensorJSON);
+free(jsonStr);
 
-        } else {
-            ESP_LOGW(TAG_http_client, "Objeto datetime no encontrado o no válido\n");
-            // return ESP_FAIL;
-        }
+} else {
+ESP_LOGW(TAG_http_client, "Objeto datetime no encontrado o no válido\n");
+// return ESP_FAIL;
+}
 
-        free(recv_http); // Libera la memoria después de analizar el JSON
-        recv_http = NULL; // Establece el puntero a NULL para evitar errores futuros
-    }
-    break;
+free(recv_http); // Libera la memoria después de analizar el JSON
+recv_http = NULL; // Establece el puntero a NULL para evitar errores futuros
+}
+break;
 default:
-    break;
+break;
 }
 return ESP_OK;
 }
@@ -126,12 +126,12 @@ return ESP_OK;
 static void rest_get()
 {
 esp_http_client_config_t config_get = {
-    .url = "http://fabrica.faniot.ar:1880/ota/firmwareversion?chip_id=C44F33605219",//"http://worldclockapi.com/api/json/utc/now",
-    .method = HTTP_METHOD_GET,
-    .cert_pem = NULL,
-    // .buffer_size = 1024,
-    .event_handler = client_event_get_handler};
-    
+.url = "http://fabrica.faniot.ar:1880/ota/firmwareversion?chip_id=C44F33605219",//"http://worldclockapi.com/api/json/utc/now",
+.method = HTTP_METHOD_GET,
+.cert_pem = NULL,
+// .buffer_size = 1024,
+.event_handler = client_event_get_handler};
+
 esp_http_client_handle_t client = esp_http_client_init(&config_get);
 esp_http_client_perform(client);
 esp_http_client_cleanup(client);
@@ -140,12 +140,12 @@ esp_http_client_cleanup(client);
 
 void get_data_time(char* _URL){
 esp_http_client_config_t config_get = {
-    .url = _URL,
-    .method = HTTP_METHOD_GET,
-    .cert_pem = NULL,
-    .buffer_size = 1024,
-    .event_handler = client_event_get_handler};
-    
+.url = _URL,
+.method = HTTP_METHOD_GET,
+.cert_pem = NULL,
+.buffer_size = 1024,
+.event_handler = client_event_get_handler};
+
 esp_http_client_handle_t client = esp_http_client_init(&config_get);
 esp_http_client_perform(client);
 esp_http_client_cleanup(client);
@@ -153,7 +153,7 @@ esp_http_client_cleanup(client);
 
 }
 // Función para crear un objeto JSON con los datos del sensor de temperatura, fecha y hora
-cJSON* createTemperatureJSON(struct sensor* sensor_data) {//cambiar nombre por jsonsito
+cJSON* add_to_JSON(struct sensor* sensor_data) {//cambiar nombre por jsonsito
 // Crear un objeto JSON
 cJSON* json = cJSON_CreateObject();
 
@@ -173,9 +173,9 @@ esp_err_t add_to_JSON(cJSON *JSON_to_send, const char *titulo, const char *value
 // Parsea el valor JSON proporcionado como una cadena
 cJSON *value_json = cJSON_Parse(value);
 if (value_json == NULL) {
-    // Si falla el análisis, registra un error y devuelve ESP_FAIL
-    ESP_LOGE(TAG, "Failed to parse value JSON");
-    return ESP_FAIL;
+// Si falla el análisis, registra un error y devuelve ESP_FAIL
+ESP_LOGE(TAG, "Failed to parse value JSON");
+return ESP_FAIL;
 }
 
 // Agrega el valor JSON parseado al objeto JSON con el título proporcionado
